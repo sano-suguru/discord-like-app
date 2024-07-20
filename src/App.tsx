94aeb170a400b19392f2e4dc267356de224be49c
sanosuguru <sa.50.00.no.riku@gmail.com>
2024-07-20 23:55:20 +0900

    ルートベースのコード分割でパフォーマンス最適化

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, Spinner } from '@chakra-ui/react';
 
-import LoginForm from './components/LoginForm';
 import { Navbar } from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
-import Chat from './pages/Chat';
-import Home from './pages/Home';
-import ProfilePage from './pages/ProfilePage';
 import { baseUrl } from './util/baseUrl';
+
+const LoginForm = lazy(() => import('./components/LoginForm'));
+const Home = lazy(() => import('./pages/Home'));
+const Chat = lazy(() => import('./pages/Chat'));
+const ProfilePage = lazy(() => import('./pages/ProfilePage'));
 
 const App: React.FC = () => {
   return (
     <ChakraProvider>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path={`${baseUrl}login`} element={<LoginForm />} />
-          <Route element={<PrivateRoute />}>
-            <Route path={baseUrl} element={<Home />} />
-            <Route path={`${baseUrl}chat`} element={<Chat />} />
-            <Route path={`${baseUrl}chat/:channelId`} element={<Chat />} />
-            <Route path={`${baseUrl}profile`} element={<ProfilePage />} />
-          </Route>
-          <Route path="*" element={<Navigate to={baseUrl} replace />} />
-        </Routes>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route path={`${baseUrl}login`} element={<LoginForm />} />
+            <Route element={<PrivateRoute />}>
+              <Route path={baseUrl} element={<Home />} />
+              <Route path={`${baseUrl}chat`} element={<Chat />} />
+              <Route path={`${baseUrl}chat/:channelId`} element={<Chat />} />
+              <Route path={`${baseUrl}profile`} element={<ProfilePage />} />
+            </Route>
+            <Route path="*" element={<Navigate to={baseUrl} replace />} />
+          </Routes>
+        </Suspense>
       </Router>
     </ChakraProvider>
   );
